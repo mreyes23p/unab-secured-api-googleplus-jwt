da23c044d565af689b5a9376eaadd1035f102435
Manuel Reyes <manuel@23people.co>
2015-06-19 01:27:30 -0300

    added validation using access_token

--- controllers/api/login/index.js
+++ controllers/api/login/index.js
@@ -4,27 +4,50 @@ var usersLib = require('../../../lib/usersLib');
 
 module.exports = function (router) {  
 
-  router.post('/', function (req, res) {
+    router.get('/', function(req, res){
 
-    var userData = req.body;    
+        //If access token was not sent in the query string ej: api/login?who=google&access_token=wrwerwqer
+        //Then return a 401
+        if(!req.query || !req.query.who || !req.query.accesstoken){
+            return res.status(401).end();
+        }
 
-    usersLib.findOne(userData.username, userData.password, function(error, user){
-
-      if (error && error.message === 'NOT_FOUND'){
-        return res.status(401).json({message: 'USER_OR_PASSWORD_NOT_FOUND'}).end();
-      }
-      
-      if (error){
-        return res.status(500).json(error).end();
-      }
-      console.log(user);
-      usersLib.getToken(user.id, function(error, token){
-        return res.status(201).json({'token': token}).end();  
-      });      
+        //Validate the access token with the third party (ej: google)
+        usersLib.validateThirdPartyAccessToken(req.query.who, req.query.accesstoken, function(error){
+            if (error){
+                return res.status(401).end();
+            }
+            
+            //Get the JWToken
+            usersLib.getToken('', function(error, token){
+                return res.status(200).json({'token': token}).end();  
+            });             
+        });
+        
 
     });
-    
-  });
+
+    router.post('/', function (req, res) {
+
+        var userData = req.body;    
+
+        usersLib.findOne(userData.username, userData.password, function(error, user){
+
+            if (error && error.message === 'NOT_FOUND'){
+                return res.status(401).json({message: 'USER_OR_PASSWORD_NOT_FOUND'}).end();
+            }
+
+            if (error){
+                return res.status(500).json(error).end();
+            }
+            console.log(user);
+            usersLib.getToken(user.id, function(error, token){
+                return res.status(201).json({'token': token}).end();  
+            });      
+
+        });
+
+    });
 
 
 };
